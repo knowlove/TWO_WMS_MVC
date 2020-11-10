85c1bb113b80ee7e8616c709821a106b3543aa70
knowlove <70941597+knowlove@users.noreply.github.com>
2020-11-10 14:12:39 +0800

    444

---
+++ Controllers/Home_pageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class Home_pageController : Controller
+    {
+        // GET: Home_page
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
