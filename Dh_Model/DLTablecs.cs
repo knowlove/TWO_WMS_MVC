c25ffc9a6be13ca2a5af752d70710a442a39a970
knowlove <70941597+knowlove@users.noreply.github.com>
2020-11-11 20:22:49 +0800

    11

---
+++ Dh_Model/DLTablecs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Dh_Model
+{
+    public class DLTablecs
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Pass { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
