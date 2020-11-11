aa0864c890b1c0bc667729ba862510b45e59667a
knowlove <70941597+knowlove@users.noreply.github.com>
2020-11-11 14:08:40 +0800

    11

---
+++ Controllers/DhController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class DhController : Controller
+    {
+        // GET: Dh
+        public ActionResult DhDj()
+        {
+            return View();
+        }
+        // GET: Dh
+        public ActionResult FbpDh()
+        {
+            return View();
+        }
+        // GET: Dh
+        public ActionResult KjDh()
+        {
+            return View();
+        }
+        // GET: Dh
+        public ActionResult DhJl()
+        {
+            return View();
+        }
+        // GET: Dh
+        public ActionResult Cgzj()
+        {
+            return View();
+        }
+    }
+}
--- Dh_Dal/DhDal.cs
+++ Dh_Dal/DhDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC.Dh_Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,26 @@ namespace MVC.Dh_Dal
 {
     public class DhDal
     {
+
+        /// <summary>
+        /// 查询所有
+        /// </summary>
+        /// <returns></returns>
+        public List<DhTable> GetDhTables() {
+
+            string sql = string.Format("select * from DhTable");
+            return DBHelper.GetList<DhTable>(sql);
+        }
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public int DelDhTable(int id) {
+
+            string sql = string.Format("delete * from DBTable where DhId = {0}",id);
+            return DBHelper.ExecuteNonQuery(sql);
+        }
+
     }
 }
