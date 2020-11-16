699a05eab1b88e8a61b8191bb1a250db52eeec3f
knowlove <70941597+knowlove@users.noreply.github.com>
2020-11-16 15:24:07 +0800

    11

---
+++ Controllers/DhEmpController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class DhEmpController : Controller
+    {
+        // GET: DhEmp
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetAll()
+        {
+             return View();
+        }
+    }
+}
