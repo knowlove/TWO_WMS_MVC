ef0a2186982b1ecace4a7075612d20b7ec8f6508
YK G <3085499591@QQ.com>
2020-11-26 14:55:59 +0800

    485

---
+++ Controllers/ChukuController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ChukuController : Controller
+    {
+        // GET: Chuku
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult renwudan()
+        {
+            return View();
+        }
+        public ActionResult jiagong()
+        {
+            return View();
+        }
+        public ActionResult zhijie()
+        {
+            return View();
+        }
+        public ActionResult Indexxq(int Q_Id)
+        {
+            ViewBag.Q_Id=Q_Id;
+            return View();
+        }
+    }
+}
